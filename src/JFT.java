f59d3bf8f89d7efbacfd4807b89ad28236e1d529
Tatyanochka <tanyasmit16@yandex.ru>
2024-02-20 19:09:16 +0300

    initial commit

---
+++ src/JFT.java
@@ -0,0 +1,10 @@
+public class JFT {
+    public static void main(String[] args) {
+        float ticketAmount = 14500F;
+        int bonus = 20;
+        int miles = (int) ticketAmount/bonus;
+        System.out.println(miles);
+
+    }
+}
+
