2d010864b9ccb71fa71dd1b3b91f4d7729e6c16a
Tatyanochka <tanyasmit16@yandex.ru>
2024-02-21 06:57:24 +0300

    reformat

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        float ticketAmount = 14500F;
+        int bonus = 20;
+        int miles = (int) ticketAmount / bonus;
+        System.out.println("Количество накопленных миль: " + miles);
+
+    }
+}
+
